5c2f1dcc0142ac57a59261f8178d75b92da2dd6e
Ross Fenning <ross@ryu.rossfenning.co.uk>
2013-03-30 00:29:49 +0000

    Refactored tagcloud code to be more generic and callable from makefile.

--- makefile
+++ makefile
@@ -1,4 +1,4 @@
-pdf: graphs uml
+pdf: graphs uml tagclouds
 	mkdir -p build
 	cp src/tex/*.tex src/tex/*.bib build/
 	cd build && pdflatex bbc-food-classification.tex
@@ -11,9 +11,23 @@ uml:
 	cp src/tex/*.tex build/
 	cd build && plantuml *
 
-graphs:
+python:
+	mkdir -p build
+	cp src/python/*.py build/
+
+csv:
 	mkdir -p build
 	cp src/csv/*.csv build/
+
+tagclouds: csv python
+	cd build && python tagcloud.py ingredients_counts.csv 1 2 ingredients_counts.png
+	cd build && python tagcloud.py asian_ingredients_counts.csv 1 2 asian_ingredients_counts.png
+	cd build && python tagcloud.py italian_ingredients_counts.csv 1 2 italian_ingredients_counts.png
+	cd build && python tagcloud.py indian_ingredients_counts.csv 1 2 indian_ingredients_counts.png
+	cd build && python tagcloud.py christmas_ingredients_counts.csv 1 2 christmas_ingredients_counts.png
+	cd build && python tagcloud.py burns_night_ingredients_counts.csv 1 2 burns_night_ingredients_counts.png
+
+graphs: csv
 	head -n 50 src/csv/ingredients_counts.csv >build/top_ingredients_counts.csv
 	head -n 50 src/csv/italian_ingredients_counts.csv >build/top_italian_ingredients_counts.csv
 	head -n 50 src/csv/typical_italian_ingredients.csv >build/top_typical_italian_ingredients.csv
--- src/python/tagcloud.py
+++ src/python/tagcloud.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
+"""Recipe Miner
 
+Usage:
+  tagcloud.py <csv_file> <tag_column> <count_column> <output_file>
+
+Options:
+  -h --help     Show this screen.
+
+"""
 import csv
+from docopt import docopt
 from pytagcloud import create_tag_image, make_tags
 
-for metric in [
-    'drinks_ingredients_counts',
-    'brunch_ingredients_counts',
-    'desserts_ingredients_counts',
-    'valentines_ingredients_counts',
-    'burns_night_ingredients_counts',
-    'christmas_ingredients_counts',
-    'ingredients_counts',
-    'italian_ingredients_counts',
-    'indian_ingredients_counts',
-    'asian_ingredients_counts'
-    ]:
+if __name__ == '__main__':
+
+    arguments = docopt(__doc__)
+
+    tag_column = int(arguments['<tag_column>'])
+    count_column = int(arguments['<count_column>'])
 
     food_counts = []
-    with open('/home/ross/documents/msc/data-mining/src/csv/{}.csv'.format(metric), 'rb') as csvfile:
+    with open(arguments['<csv_file>'], 'rb') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-        for food_id, food_title, food_count in reader:
-            count_tuple = (unicode(food_title, 'utf-8'), int(food_count))
+        for row in reader:
+            count_tuple = (unicode(row[tag_column], 'utf-8'), int(row[count_column]))
             food_counts.append(count_tuple)
             if len(food_counts) >= 150:
                 break
 
     tags = make_tags(food_counts, minsize=10, maxsize=100)
-    create_tag_image(tags, '{}.png'.format(metric), size=(1024, 768), layout=4, fontname='Lobster')
+    create_tag_image(tags, arguments['<output_file>'], size=(1024, 768), layout=4, fontname='Lobster')
+
+
